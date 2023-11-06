87f3eaa870e8c50195df8f96d082afbefee82d1c
Administrator <Administrator@BLRTRAINING1.COM>
2023-11-06 16:02:54 +0530

    Widget functionality added

--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ import Widget from '@/components/Widget'
 export default function Home() {
   return (
       <>
-        <header className='h-[320px] bg-[url(../public/images/clouds.jpg)] bg-center bg-cover'>
-          <div className='flex flex-col items-center justify-center h-full'>
+        <header className='h-[320px] bg-[url(../public/images/clouds.jpg)] bg-center bg-cover border-t'>
+          <div className='flex flex-col items-center h-full mt-40'>
             <Widget />
           </div>
         </header>
--- components/Widget.js
+++ components/Widget.js
@@ -1,6 +1,22 @@
-import React from 'react'
+"use client"
+import React, {useState} from 'react'
+import { FaBusSimple, FaPlaneUp, FaHotel, FaChessBoard } from "react-icons/fa6";
+
 
 const Widget = () => {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('All');
+
+  const bringSuggestion = () => {
+    setDropdownVisible(!dropdownVisible)
+  }
+
+  const hideSuggestion = () => {
+    setDropdownVisible(false);
+  };
+
+  
   return (
     <>
         <form className="flex flex-col w-[75%]">
@@ -11,32 +27,36 @@ const Widget = () => {
                 placeholder='Search for the name of a bus service, flight service or a hotel'
                 autoComplete='off'
                 className='w-full h-[40px] border border-solid rounded-l-3xl shadow-lg ps-5'
+                onChange = {(e) => setSearchQuery(e.target.value)}
+                onClick={bringSuggestion}
             />
 
             <input 
-                type="submit"
+                type="button"
                 className='h-[40px] px-20 rounded-r-3xl cursor-pointer bg-gray-200 hover:bg-gray-300 border-l border-gray-400'
+                value="Submit"
+                onClick={hideSuggestion}
             />
           </div>
 
-          <section className="bg-white w-[81%] ml-[1.7%] flex items-center py-5 px-10">
-            <aside className='flex items-center mr-10'>
-              <input type="radio" name="category" value="Buses" />
-              <label className="ml-2">Buses</label>
+          {dropdownVisible && <section className="bg-white w-[81%] ml-[1.7%] flex items-center py-5 px-10">
+            <aside className='flex items-center mr-4'>
+              <input type="radio" name="category" value="Buses" onChange={(e) => setSelectedCategory(e.target.value)} />
+              <label className="grid items-center grid-cols-2 gap-3 ml-2">Buses <FaBusSimple /> </label>
             </aside>
-            <aside className='flex items-center mr-10'>
-              <input type="radio" name="category" value="Flights" />
-              <label className="ml-2">Flights</label>
+            <aside className='flex items-center mr-4'>
+              <input type="radio" name="category" value="Flights" onChange={(e) => setSelectedCategory(e.target.value)} />
+              <label className="grid items-center grid-cols-2 gap-3 ml-2">Flights <FaPlaneUp /> </label>
             </aside>
-            <aside className='flex items-center mr-10'>
-              <input type="radio" name="category" value="Hotels" />
-              <label className="ml-2">Hotels</label>
+            <aside className='flex items-center mr-4'>
+              <input type="radio" name="category" value="Hotels" onChange={(e) => setSelectedCategory(e.target.value)} />
+              <label className="grid items-center grid-cols-2 gap-3 ml-2">Hotels <FaHotel /></label>
             </aside>
-            <aside className='flex items-center mr-10'>
-              <input type="radio" name="category" value="All" />
-              <label className="ml-2">All</label>
+            <aside className='flex items-center mr-4'>
+              <input type="radio" name="category" value="All" onChange={(e) => setSelectedCategory(e.target.value)} />
+              <label className="grid items-center grid-cols-2 gap-3 ml-2">All <FaChessBoard /></label>
             </aside>
-          </section>
+          </section>}
         </form>
     </>
   )
